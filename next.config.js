d5591b7988015b5766a7ccc9d8dc81a6558d9ed0
msanchezgrice <msanchezgrice@gmail.com>
2025-03-15 10:02:12 -0500

    Fix: Update Next.js configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  distDir: '.next',
 };
 
 module.exports = nextConfig; 
